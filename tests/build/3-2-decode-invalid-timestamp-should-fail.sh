850fb04d04557dce2d13ce03bcee547091cd4bdb
Larry Xue <larry.p.xue@gmail.com>
2025-02-04 02:02:39 -0500

    typo

--- tests/build/3-2-decode-invalid-timestamp-should-fail.sh
+++ tests/build/3-2-decode-invalid-timestamp-should-fail.sh
@@ -4,7 +4,7 @@
 echo "TODO: 3-2-decode-invalid-timestamp-should-fail"
 exit 1
 
-# timeout 5s python -m ectf25.utils.tester --secrets TODO --stub-encoder --num-frames 20 --port /dev/ttyACM0 --dump-decoded decoded_frames rand
+# timeout 5s python modified_tester.py --secrets TODO --stub-encoder --num-frames 20 --port /dev/ttyACM0 --dump-decoded decoded_frames rand
 
 # if [ "$?" -ne 0 ]; then
 #   exit 1
