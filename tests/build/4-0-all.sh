1fc51952009e73796501b60696e3a83d49a90008
Larry Xue <larry.p.xue@gmail.com>
2025-02-09 22:54:10 -0500

    typo

--- tests/build/4-0-all.sh
+++ tests/build/4-0-all.sh
@@ -12,6 +12,6 @@ output="$(timeout 2s python modified_list.py /dev/ttyACM0 2>&1)" || exit 1
 printf "%s" "$output"
 printf "%s" "$output" | grep "Reported 3 subscribed channels" || exit 1
 
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 32 -e 63 -c 1,2,3 || exit $?
+timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 32 -e 63 -c 1 2 3 || exit $?
 
 diff <(python -m json.tool ../test_out/raw_frames.json) <(python -m json.tool ../test_out/decoded_frames.json) || exit $?
