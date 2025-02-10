06d5dcf5472294c709786fe6f6620e08ffe74e76
Larry Xue <larry.p.xue@gmail.com>
2025-02-09 22:54:52 -0500

    commit

--- tests/build/2-3-decode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 32 -e 63 -c 1 || exit $?
-
-diff <(python -m json.tool ../test_out/raw_frames.json) <(python -m json.tool ../test_out/decoded_frames.json) || exit $?
-
-
-# rm decoded_frames
