ba72fab8b24b144b92bc3ee689fc0cadd8134200
Larry Xue <larry.p.xue@gmail.com>
2025-02-09 21:44:16 -0500

    change tests

--- tests/build/2-2-decode.sh
+++ tests/build/2-2-decode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 32 -e 63 -c 1 || exit $?
+timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json json ../frames/x_c0.json || exit $?
 
 diff <(python -m json.tool ../test_out/raw_frames.json) <(python -m json.tool ../test_out/decoded_frames.json) || exit $?
 
