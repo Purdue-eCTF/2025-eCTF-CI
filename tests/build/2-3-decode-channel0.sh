6954fab2bc3e460945e02e1a9cb1861c5bc3e79f
Larry Xue <larry.p.xue@gmail.com>
2025-02-10 21:36:20 -0500

    fix test again again

--- tests/build/2-3-decode-channel0.sh
+++ tests/build/2-3-decode-channel0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 64 -e 127 -c 0 || exit $?
+timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 --dump-raw ../test_out/raw_frames.json --dump-decoded ../test_out/decoded_frames.json rand2 -s 64 -e 95 -c 0 || exit $?
 
 diff <(python -m json.tool ../test_out/raw_frames.json) <(python -m json.tool ../test_out/decoded_frames.json) || exit $?
 
--- tests/build/3-2-decode-invalid-timestamp.sh
+++ tests/build/3-2-decode-invalid-timestamp.sh
@@ -4,7 +4,7 @@
 timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 40 -e 40 -c 1 && exit 1
 
 # invalid timestamps (outside subscription)
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 128 -e 128 -c 1 && exit 1
+timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 96 -e 96 -c 1 && exit 1
 
 
 # if [ "$?" -ne 0 ]; then
