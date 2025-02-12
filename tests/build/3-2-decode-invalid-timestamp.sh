01b3c931affd3e6514d51a9aa3778ed198a0e68c
Larry Xue <larry.p.xue@gmail.com>
2025-02-12 01:24:42 -0500

    fix shell scripting bug

--- tests/build/3-2-decode-invalid-timestamp.sh
+++ tests/build/3-2-decode-invalid-timestamp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # invalid timestamps (non-increasing)
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 40 -e 40 -c 1 && exit 1
+if timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 40 -e 40 -c 1; then
+	exit 1
+fi
 
 # invalid timestamps (outside subscription)
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 96 -e 96 -c 1 && exit 1
+if timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 96 -e 96 -c 1; then
+	exit 1
+fi
+
+exit 0
--- tests/build/3-3-decode-invalid-channel.sh
+++ tests/build/3-3-decode-invalid-channel.sh
@@ -2,4 +2,9 @@
 
 rm -f ../test_out/subscription.bin
 timeout 10s python -m ectf25_design.gen_subscription ../test_out/global.secrets ../test_out/subscription.bin 0xDEADBEEF 128 160 1 || exit $?
-timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 130 -e 130 -c 2 && exit 1
+
+if timeout 5s python modified_tester.py --secrets ../test_out/global.secrets --timing --port /dev/ttyACM0 rand2 -s 130 -e 130 -c 2; then
+	exit 1
+fi
+
+exit 0
