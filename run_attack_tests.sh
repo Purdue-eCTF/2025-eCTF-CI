d45150ebfff0c3c6ad3694a9aea904e2b1245bfa
Larry Xue <larry.p.xue@gmail.com>
2025-03-05 23:00:42 -0500

    attack test fix

--- run_attack_tests.sh
+++ run_attack_tests.sh
@@ -12,13 +12,13 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" || exit
 read -r IP CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT < ports.txt
 export IP CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT
 
-for test in tests/attack/"$1"/*.sh; do
+for test in tests/attack/global/*.sh tests/attack/"$1"/*.sh; do
     if [[ $test == tests/attack/x-*.sh ]]; then
         continue
     fi
     echo -e "${C_MEDIUMPURPLE1}${F_BOLD}Running test $test${NO_FORMAT}"
     bash "$test" 2>&1 | tee temp_output
-    grep -aEo "ectf\{[a-zA-Z0-9_]+\}" temp_output | sed "s/^/POTENTIAL FLAG: /"
+    grep -aEo "[a-z0-9]{16}^ flag ^" temp_output | sed "s/^/POTENTIAL FLAG: ectf{$1_/"
 done
 
 rm temp_output
