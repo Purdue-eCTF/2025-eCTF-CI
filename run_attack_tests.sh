0c67ee9a56cda18ec1bcb328588d66faede791ea
Larry Xue <larry.p.xue@gmail.com>
2025-03-18 18:23:01 -0400

    setup_attacks.sh

--- run_attack_tests.sh
+++ run_attack_tests.sh
@@ -9,16 +9,9 @@ fi
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" || exit
 . ../.venv/bin/activate
-if [[ -f ../test_out/design/setup.py ]]; then
-    pip uninstall ectf25_design
-else
-    pip install -e ../test_out/design/ || pip uninstall ectf25_design
-fi
 
-read -r IP CHANNEL_0_PORT CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT < ../test_out/ports.txt
-export IP CHANNEL_0_PORT CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT
+. setup_attacks.sh
 export LOGURU_LEVEL=INFO
-export PYTHONPATH=$PWD
 
 trap "rm -f temp_output" EXIT
 for test in tests/attack/*/*; do
--- tests/attack/0-global/0-all-subscriptions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-
-timeout 15s python -m ectf25.tv.subscribe ../test_out/own.sub /dev/ttyACM0 || exit $?
-timeout 15s python -m ectf25.tv.subscribe ../test_out/expired.sub /dev/ttyACM0 || exit $?
