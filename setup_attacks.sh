8e4b62d56f872151ef3b90bf3137adcb453528c2
Larry Xue <larry.p.xue@gmail.com>
2025-03-18 18:43:56 -0400

    fix setup_attacks.sh

--- setup_attacks.sh
+++ setup_attacks.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 echo Setting up attacks
-if [ -f ../test_out/design/setup.py ]; then
-    pip uninstall ectf25_design
-else
+pip uninstall ectf25_design
+if [[ ! -f ../test_out/design/setup.py ]]; then
     pip install -e ../test_out/design/ || pip uninstall ectf25_design
 fi
 
 read -r IP CHANNEL_0_PORT CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT < ../test_out/ports.txt
 export IP CHANNEL_0_PORT CHANNEL_1_PORT CHANNEL_2_PORT CHANNEL_3_PORT CHANNEL_4_PORT
 
-export PYTHONPATH="$PWD"
+PYTHONPATH="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd)"
+export PYTHONPATH
 
 timeout 15s python -m ectf25.tv.subscribe ../test_out/own.sub /dev/ttyACM0 || exit $?
 timeout 15s python -m ectf25.tv.subscribe ../test_out/expired.sub /dev/ttyACM0 || exit $?
